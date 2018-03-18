9b1d3d3f318996f3206241d013aa81f9f2d3fd40
Ndabezinhle Ndlovu <ndlovun90@gmail.com>
2018-03-18 21:49:58 +0200

    Lab2 node.js

---
+++ index.js
@@ -0,0 +1,9 @@
+let path = require('path');
+let express = require('express');
+let app = express();
+let mainRouter = require('./mainRoutes');
+
+app.use('/', mainRouter);
+
+app.listen(process.env.PORT || 3000);
+console.log("Express server running on port 3000");
